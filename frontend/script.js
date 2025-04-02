34cedb91db0e8f2c53ef5f175aceabf92b34e6c8
BartekJ0110 <bj311137@student.polsl.pl>
2025-04-02 18:29:02 +0200

    front for front

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@ const prompt = document.getElementById('prompt')
 const generateButton = document.getElementById('generate')
 const output = document.getElementById('output')
 
-const url = "http://localhost:80/generate"
+const url = "http://fastapi:8000/generate"
 
 generateButton.addEventListener('click', e => {
     fetch(url, {
